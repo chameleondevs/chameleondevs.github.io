78e92e60fc56e681f3bcfb9a2a4dee4f9ba3a20e
chameleondevs <132445033+chameleondevs@users.noreply.github.com>
2024-07-22 15:31:21 +0200

    Add files via upload

--- dynamic-form-loader/datalayer-logger.js
+++ dynamic-form-loader/datalayer-logger.js
@@ -29,24 +29,68 @@ class dataLayerLogger extends HTMLElement {
             background: rgb(28, 39, 58);
             overflow: hidden;
         }
+        .timestamp {
+            font-size: 8px;
+            color: grey;
+            text-align: right;
+            margin-top: 5px;
+        }
         summary {
             cursor: pointer;
             font-weight: bold;
             font-size: 12px;
             padding: 5px 0;
         }
+        .toggleContainer {
+          display: flex;
+          gap: 10px;
+          margin-bottom: 10px;
+        }
+        .toggle {
+          display: flex;
+          align-items: center;
+          background: rgb(28, 39, 58);
+          padding: 5px;
+          border-radius: 16px;
+          cursor: pointer;
+        }
+        .toggle input {
+          display: none;
+        }
+        .toggle span {
+          padding: 5px;
+          border-radius: 16px;
+        }
+        .toggle:hover input:not(:checked) + span {
+            color: white;
+        }
+        .toggle input:checked + span {
+          background: white;
+          color:  rgb(17, 24, 37); 
+        }
+        
       </style>
-     <div class="logContainer">
-     </div>
+      <div class="toggleContainer">
+        <label class="toggle">
+          <input type="checkbox" id="toggleDataLayer" checked>
+          <span>dataLayer</span>
+        </label>
+        <label class="toggle">
+          <input type="checkbox" id="toggleInternal" checked>
+          <span>internal</span>
+        </label>
+      </div>
+      <div class="logContainer"></div>
     `;
   }
 
   connectedCallback() {
     window.addEventListener('datalayerpush', this.handlePush.bind(this));
     window.addEventListener('cleardatalayerlogs', this.clearLogs.bind(this));
-
     window.addEventListener('message', this.handlePostMessage.bind(this));
 
+    this.shadowRoot.querySelector('#toggleDataLayer').addEventListener('change', this.filterLogs.bind(this));
+    this.shadowRoot.querySelector('#toggleInternal').addEventListener('change', this.filterLogs.bind(this));
   }
 
   disconnectedCallback() {
@@ -55,7 +99,7 @@ class dataLayerLogger extends HTMLElement {
   }
 
   handlePush(e) {
-      this.writeDataLayerPush(e.detail);
+    this.writeDataLayerPush(e.detail);
   }
 
   handlePostMessage(e) {
@@ -68,41 +112,59 @@ class dataLayerLogger extends HTMLElement {
   writeDataLayerPush(data) {
     const logContainer = this.shadowRoot.querySelector('.logContainer');
     const logItem = document.createElement('details');
-    logItem.classList.add('logItem');
+    logItem.classList.add('logItem', 'dataLayer');
     logItem.innerHTML = this.formatMessage(data.event, data, 'dataLayer');
     logContainer.appendChild(logItem);
+    this.filterLogs();
   }
-
 
   writeMessage(data) {
     const logContainer = this.shadowRoot.querySelector('.logContainer');
     const messageItem = document.createElement('details');
-    messageItem.classList.add('messageItem');
+    messageItem.classList.add('messageItem', 'internal');
     try {
       const eventInfoArray = data.split(/:(.+)/);
       const eventType = eventInfoArray[0];
       const parsedData = JSON.parse(eventInfoArray[1]);
       messageItem.innerHTML = this.formatMessage(eventType, parsedData, 'internal');
       logContainer.appendChild(messageItem);
+      this.filterLogs();
     } catch (e) {
-      console.error(`Could not parse message ${JSON.stringify(data)}`);
+      // console.error(`Could not parse message ${JSON.stringify(data)}`);
     }
   }
 
   formatMessage(eventType, data, source) {
     let html = `<summary>${eventType} <small>${source}</small></summary>`;
     html += `${this.formatData(data)};`
+    html += `<div class="timestamp">timestamp: ${Date.now()}</div>`;
     return html;
   }
 
   formatData(data) {
     return Object.keys(data).map(key => {
-        return `<strong>${key}</strong>: ${typeof data[key] === 'string' || typeof data[key] === 'number' ? data[key] : JSON.stringify(data[key])}`;
+      return `<strong>${key}</strong>: ${typeof data[key] === 'string' || typeof data[key] === 'number' ? data[key] : JSON.stringify(data[key])}`;
     }).join('<br>');
   }
 
   clearLogs() {
     this.shadowRoot.querySelector('.logContainer').innerHTML = '';
+  }
+
+  filterLogs() {
+    const showDataLayer = this.shadowRoot.querySelector('#toggleDataLayer').checked;
+    const showInternal = this.shadowRoot.querySelector('#toggleInternal').checked;
+
+    const logItems = this.shadowRoot.querySelectorAll('.logItem');
+    const messageItems = this.shadowRoot.querySelectorAll('.messageItem');
+
+    logItems.forEach(item => {
+      item.style.display = showDataLayer ? 'block' : 'none';
+    });
+
+    messageItems.forEach(item => {
+      item.style.display = showInternal ? 'block' : 'none';
+    });
   }
 }
 
